51463e8ac21b5e84c43d3fd9784c763e35c5cd83
javency <635142482@qq.com>
2018-01-26 18:19:25 +0800

    master

--- js/index.js
+++ js/index.js
@@ -11,20 +11,31 @@ $(function() {
 })
 
 $(function() {
-    // $.ajax({
-    //     type: "get",
-    //     async: false,
-    //     data: "",
-    //     dataType : "json",
-    //     contentType: "application/json;charset=UTF-8",
-    //     url: 'http://' + changeUrl.address + '/Class_Course_api?whereFrom=Search_Course',
-    //     success: function(msg) {
-    //         $(".prepareLesson_title").eq(0).text(msg.data[0].CourseName);
-    //         $(".prepareLesson_subject").eq(0).text(msg.data[0].CourseSubject + "：" + msg.data[0].CourseName)
-    //         $(".ClassBegins").eq(0).text(msg.data[0].ClassBegins)
-    //     },
-    //     error: function(msg) {
-    //         console.log("error:" + msg)
-    //     }
-    // })
+    var CourseContainer = $("#CourseContainer")
+    $.ajax({
+        type: "get",
+        async: false,
+        data: "",
+        dataType : "json",
+        contentType: "application/json;charset=UTF-8",
+        url: 'http://' + changeUrl.address + '/Class_Course_api?whereFrom=Search_Course',
+        success: function(msg) {
+            // console.log(msg.data)
+            // CourseContainer.append(`
+            // <div class="col-md-3 col-sm-6">
+            //     <a href="./detailLesson.html" target="_">
+            //          <div class="Course borderRadio4">
+            //             <img src="./images/CourseImage01.jpg" alt="">
+            //             <p class="Couse_title oneline">${msg.data[0].CourseName}</p>
+            //          </div>
+            //      </a>
+            //     <p class="mtb5  oneline"><span class="CourseName">${msg.data[0].CourseSubject}</span></p>
+            //     <p class="mtb5">开课时间：<span class="CourseTime">${msg.data[0].ClassBegins}</span></p>
+            // </div>
+            // `)
+        },
+        error: function(msg) {
+            console.log("error:" + msg)
+        }
+    })
 })
