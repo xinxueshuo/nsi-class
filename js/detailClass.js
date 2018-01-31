e31324522def94a60fa192232fe87804ebf5e205
javency <635142482@qq.com>
2018-01-31 18:19:14 +0800

    改换classCenter名字  课程详情页更改

--- js/detailClass.js
+++ js/detailClass.js
@@ -14,7 +14,7 @@ function buyNow() {
             dataType: "jsonp", //数据类型为jsonp  
             jsonp:   "Callback", //服务端用于接收callback调用的function名的参数  
             success: function(msg) {
-                console.log(1)
+                console.log("Buy:" + msg)
             }
         })
     })
@@ -59,27 +59,67 @@ function isLogin() {
 }
 isLogin();
 
-function lessonDsec() {
-    var $tab01 = $("#tab01"),
-        $tab02 = $("#tab02"),
-        $tab03 = $("#tab03"),
-        $tab04 = $("#tab04")
+
+$(function() {
+    //获取url地址问号后面部分
+    function getQueryStringArgs() {
+        var qs = location.search.length > 0 ? location.search.substring(1) : '',
+            args = {},
+            items = qs.length ? qs.split('&') : [],
+            item = null,
+            name = null,
+            value = null,
+            i = 0,
+            len = items.length;
+        for (i = 0; i < len; i++) {
+            item = items[i].split('=');
+            name = decodeURIComponent(item[0]);
+            value = decodeURIComponent(item[1]);
+            name = item[0];
+            value = item[1];
+
+            if (name.length) {
+                args[name] = value;
+            }
+        }
+        return args;
+    }
+    var args = getQueryStringArgs(),
+        Id = decodeURIComponent(args['Id'])
+
+    function lessonDsec() {
+        var $tab01 = $("#tab01"),
+            $tab02 = $("#tab02"),
+            $tab03 = $("#tab03"),
+            $tab04 = $("#tab04")
+        $.ajax({
+            type: "get",
+            dataType: 'json',
+            contentType: "application/json;charset=UTF-8",
+            data: {
+                "Id": Id
+            },
+            url: 'http://' + changeUrl.address + '/Class_Course_api?whereFrom=Course_GetHtml',
+            success(msg) {
+                console.log(msg)
+                $tab01.html(msg.data[0].Html01)
+                $tab02.html(msg.data[0].Html02)
+                $tab03.html(msg.data[0].Html03)
+                $tab04.html(msg.data[0].Html04)
+            }
+        })
+    }
+    lessonDsec()
     $.ajax({
-        type: "post",
-        async: false,
-        dataType: 'jsonp',
-        jsonp: 'Callback',
-        data: {
-            "Id": $("#ClassId").text()
-        },
-        url: 'http://' + changeUrl.address + '/Class_Course_api?whereFrom=Course_GetHtml',
-        success(msg) {
-            // console.log(msg[0].Html01)
-            $tab01.html(msg[0].Html01)
-            $tab02.html(msg[0].Html02)
-            $tab03.html(msg[0].Html03)
-            $tab04.html(msg[0].Html04)
+        type: "get",
+        dataType: "json",
+        data: { Id: Id },
+        url: 'http://' + changeUrl.address + '/Class_Course_api?whereFrom=Search_Course',
+        success: function(msg) {
+            console.log(msg.data[0])
+            $("#CourseName").text(msg.data[0].CourseName);
+            $("#ClassBegins").text(msg.data[0].ClassBegins);
+            $("#CoursePrice").text(msg.data[0].CoursePrice)
         }
     })
-}
-lessonDsec()
+})
