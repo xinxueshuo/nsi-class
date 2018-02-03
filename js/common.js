e59d380144ca61cd40bdb8b1b7c714ad2e2a1482
javency <635142482@qq.com>
2018-02-03 22:57:14 +0800

    首页修改导航条样式以及链接

---
+++ js/common.js
@@ -0,0 +1,28 @@
+$(function() {
+    function isLogin() {
+        var $login = $("#login")
+        if ($.cookie('username') === undefined) {
+            $login.text("登录")
+        } else {
+            $login.text($.cookie('username'))
+        }
+    }
+    isLogin();
+})
+
+// 回到顶部
+$(function() {
+    var backtoTop = $("#backtoTop")
+    $(window).scroll(function() {
+        var sc = $(window).scrollTop();
+        if (sc > 0) {
+            backtoTop.fadeIn(200)
+        } else {
+            backtoTop.fadeOut(200)
+        }
+    })
+
+    backtoTop.on("click", function() {
+        $('body,html').animate({ scrollTop: 0 }, 500);
+    })
+})
